dbf288a5f213ccf476ee3f1deb87a50ac3a99908
kakeru321 <shoya0609bnnmn@gmail.com>
2021-03-21 20:31:49 +0900

    20210321_interstitial2

--- lib/screens/base_page.dart
+++ lib/screens/base_page.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:math' as math;
 import 'package:admob_flutter/admob_flutter.dart';
 import 'package:firebase_admob/firebase_admob.dart';
@@ -26,6 +27,24 @@ class _BasePageState extends State<BasePage> {
     });
   }
 
+  static String getAppId() {
+    if (Platform.isIOS) {
+      return 'ca-app-pub-7104775285154830~1946964690';
+    } else if (Platform.isAndroid) {
+      return 'ca-app-pub-7104775285154830~1946964690';
+    }
+    return null;
+  }
+
+  static String getInterstitialAdUnitId() {
+    if (Platform.isIOS) {
+      return 'ca-app-pub-7104775285154830/5999235244';
+    } else if (Platform.isAndroid) {
+      return 'ca-app-pub-7104775285154830/9525215581';
+    }
+    return null;
+  }
+
   MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
     keywords: <String>['flutterio', 'beautiful apps'],
     contentUrl: 'https://flutter.io',
@@ -37,12 +56,15 @@ class _BasePageState extends State<BasePage> {
   Widget build(BuildContext context) {
     var interstitialRandom = math.Random().nextInt(5);
     var correct = 0;
+    print(interstitialRandom);
     if (interstitialRandom == correct) {
+      FirebaseAdMob.instance.initialize(appId: getAppId());
+
       InterstitialAd myInterstitial = InterstitialAd(
         // Replace the testAdUnitId with an ad unit id from the AdMob dash.
         // https://developers.google.com/admob/android/test-ads
         // https://developers.google.com/admob/ios/test-ads
-        adUnitId: InterstitialAd.testAdUnitId,
+        adUnitId: getInterstitialAdUnitId(),
         targetingInfo: targetingInfo,
         listener: (MobileAdEvent event) {
           print("InterstitialAd event is $event");
